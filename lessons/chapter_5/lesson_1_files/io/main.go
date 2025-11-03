625be106eb475c4fdf48fd175fcd99e0146077e8
sidbro_archpc <sasha.sidenko09@gmail.com>
2025-11-03 16:29:55 +0300

    completed lesson

--- lessons/chapter_5/lesson_1_files/io/main.go
+++ lessons/chapter_5/lesson_1_files/io/main.go
@@ -1,1 +1,42 @@
 package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+// Копирование
+func ioCopy() {
+	src := strings.NewReader("Vodolaz\n")
+
+	// В качестве Writer используем консоль
+	written, err := io.Copy(os.Stdout, src)
+
+	if err != nil {
+		fmt.Println("Copy error:", err)
+		return
+	}
+
+	fmt.Println("Successful copied", written, "bytes")
+}
+
+// Считывание всех данных
+func ioReadAll() {
+	src := strings.NewReader("Po komande pacan pernut hochet")
+
+	data, err := io.ReadAll(src)
+
+	if err != nil {
+		fmt.Println("Read all error:", err)
+		return
+	}
+
+	fmt.Println(string(data))
+}
+
+func main() {
+	ioCopy()
+	ioReadAll()
+}
